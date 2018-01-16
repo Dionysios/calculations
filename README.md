6356e13e40006c77bf52490d24e9d6b41fe7cf0a
Dionysios Papathanopoulos <dpapathanopoulos@webst.gr>
2018-01-16 22:30:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# calculation
+
+
+Simple javascript calculations pages
---
+++ js/script.js
@@ -0,0 +1,16 @@
+var row;
+var col;
+var MultiArray = [row][col];
+
+//MultiArray = new Array(5);
+
+//MultiArray[0] = new Array(5);
+
+for (var i=0; i<4; i++) {
+    for (var j=0; j<4; j++) {
+        MultiArray[0][0];
+    }
+ }
+
+ console.log( matrix);
+
